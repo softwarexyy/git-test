94c55ff4eca49735865a9c715491681aa430af4d
yancy <none>
2018-12-03 14:42:34 +0800

    给main方法增加一个语句

--- src/com/yancy/Main.java
+++ src/com/yancy/Main.java
@@ -9,5 +9,6 @@ public class Main {
 
 	public static void main(String []args) {
 		System.out.println("hello git!");
+		System.out.println(" ___________________ This is modified by yancy __________");
 	}
 }
