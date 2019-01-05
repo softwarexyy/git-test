fab26f7fd347874012c7d54f013e83d2ae37005f
softwarexyy <softwarexyy@126.com>
2019-01-05 14:41:26 +0800

    test

--- src/com/yancy/Main.java
+++ src/com/yancy/Main.java
@@ -10,5 +10,6 @@ public class Main {
 	public static void main(String []args) {
 		System.out.println("hello git!");
 		System.out.println(" ___________________ This is modified by yancy __________");
+		System.out.println();
 	}
 }
